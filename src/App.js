72254b7ae73d4b306e73cf353b5a5854705aa606
ramanamurthy-bure <ramanamurthy.bure@gmail.com>
2022-11-04 15:33:24 +0530

    Redux Example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+Redux In React JS
+npm install redux
+npm install react-redux
+npm install redux-thunk
+npm install redux-devtools-extension
+-> It is all about managing state values in multiple components at a time, using redux store
 
-function App() {
+Store: Combined application state from all components(BigJavaScript object)
+Provider : Provides the state data to all components
+Components : Encapsulates the main component
+Actions->Reducers->Store
+
+//We can add these lines for incrementing it by 1 
+<button onClick={IncAction()}>Increment</button>
+<button onClick={DecAction()}>Decrement</button>
+*/
+
+import React from 'react'
+import { connect } from 'react-redux';
+import { IncAction,DecAction } from './action';
+
+export const App = ({local_variable,IncAction,DecAction}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>{local_variable}</h1>
+        <button onClick={()=>IncAction(5)}>Increment</button>
+        <button onClick={()=>DecAction(5)}>Decrement</button>
+      </center>
     </div>
-  );
+  )
 }
-
-export default App;
+const mapStateToProps=state=>(
+  {
+    local_variable : state
+  }
+)
+export default connect(mapStateToProps,{IncAction,DecAction})(App);
